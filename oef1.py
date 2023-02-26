d4f79ffb3a927a4f00571a45c77cbaba9bc565ce
stevenghekiere-sg <76452850+stevenghekiere-sg@users.noreply.github.com>
2023-02-26 22:53:36 +0100

    Update oef1.py

--- oef1.py
+++ oef1.py
@@ -1,1 +1,5 @@
-# In deze oefening 
+# Maak een programma dat op het scherm de tuples weergeeft van de accelerometer (met de REPL) (gebruik de print() functie hiervoor.
+# Denk aan een leesbare verversing van het display. (het heeft geen zin om het display 100x per seconde te verversen)
+
+from microbit import *
+
